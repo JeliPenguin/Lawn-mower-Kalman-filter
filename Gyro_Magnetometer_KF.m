d5cada97d38108f8ebf38cc02be031ce97e4679b
JeliPenguin <jeffbenchu@gmail.com>
2024-02-06 14:40:57 +0000

    completed gyro magnet kf

--- Gyro_Magnetometer_KF.m
+++ Gyro_Magnetometer_KF.m
@@ -1,53 +1,65 @@
 function gyro_mag_heading_solution = Gyro_Magnetometer_KF(dr_measurement_data,times)
-% Calculates an gyro-smoothed magnetic heading solution using Kalman
-% filtering
-% Inputs:
-%   dr_measurement_data                      Array of raw dead reckoning measurement data
-%   times                                    Array of time value for each epoch
-%
-% Outputs:
-%   gyro_mag_heading_solution                Calculated Heading Solution
-%                                               Col 1: Heading (deg)
 
 Define_Constants;
 
 dr_measurement = dr_measurement_data(:,2:end);
 
-% Initialization
-Q = 0.01;  % Process noise covariance for gyroscope
-R_k = sigma_mh^2;  % Measurement noise covariance for magnetometer
+epoch_num = height(times);
 
-% Initial state estimate and covariance
-x_hat = 0;      % Initial heading estimate
-P = 1;          % Initial covariance estimate
+P_k_m_1_plus = [sigma_gyro^2,0
+                0,1];
+
+
+x_caret_k_m_1_plus = zeros(2,1);
+
+% Compute transition matrix
+Phi_k_m_1 = [1,tau
+             0,1];
+
+% Compute system noise covariance matrix
+Q_k_m_1 = [S_rg*tau+(S_bgd*tau^3)/3,0.5*S_bgd*tau^2
+           0.5*S_bgd*tau^2,S_bgd*tau
+           ];
+
+% Compute measurement matrix 
+H_k = [-1,0];
+
+% Compute measurement noise covariance
+R_k = sigma_mh^2;
 
 gyro_data = dr_measurement(:,5); 
 mag_data = dr_measurement(:,6)*deg_to_rad;
 
-% Simulation parameters
-epoch_num = height(times);
-
 gyro_mag_heading_solution = zeros(epoch_num,1);
-gyro_mag_heading_solution(1) = mag_data(1);
+gyro_mag_heading_solution(1) = mag_data(1)*rad_to_deg;
+psi_k_g = mag_data(1);
+for i = 2:epoch_num
 
-% Kalman Filter Loop
-for k = 2:epoch_num
-    % Prediction Step (state and covariance prediction)
-    x_hat_minus = x_hat + gyro_data(k-1) * tau;
-    P_minus = P + Q;
-    
-    % Update Step (Kalman gain, measurement residual, and update state and covariance)
-    K = P_minus * inv(P_minus + R_k);
-    delta_z = mag_data(k) - x_hat_minus;
-    x_hat = x_hat_minus + K * delta_z;
-    P = (1 - K) * P_minus;
-    
-    % Save the filtered heading estimate
-    gyro_mag_heading_solution(k) = x_hat*rad_to_deg;
+    psi_k_M = mag_data(i);
+    psi_k_g = psi_k_g+gyro_data(i)*tau;
+
+    % Propagate state estimates and error covariance matrix
+    x_caret_k_minus = Phi_k_m_1*x_caret_k_m_1_plus;
+    P_k_minus = Phi_k_m_1*P_k_m_1_plus*Phi_k_m_1'+Q_k_m_1;
+
+    % Compute Kalman gain matrix
+    K_k = P_k_minus*H_k'*inv(H_k*P_k_minus*H_k'+R_k);
+
+    % Formulate measurement innovation vector
+    delta_z_m_k = psi_k_M-psi_k_g+x_caret_k_minus(1);
+
+    % Update state estimates and error covaraince matrix
+    x_caret_k_m_1_plus = x_caret_k_minus + K_k*delta_z_m_k;
+    P_k_m_1_plus = (eye(2) - K_k*H_k)*P_k_minus;
+
+    % Apply correction to solution of current epoch
+    correction = psi_k_g-x_caret_k_m_1_plus(1);
+    gyro_mag_heading_solution(i) = correction*rad_to_deg;
+
 end
 
 t = (1:epoch_num) * tau;
-f = figure();
+f = figure("Visible","off");
 plot(t, mag_data*rad_to_deg, 'b', t, gyro_mag_heading_solution, 'r');
 legend('Magnetometer Data', 'Filtered Heading');
 xlabel('Time (s)');
--- main.m
+++ main.m
@@ -11,6 +11,7 @@ gnss_velocity_e = gnss_solution(:,6);
 
 %% Gyro-smoothed Magnetic Heading solution
 gyro_mag_heading_solution = Gyro_Magnetometer_KF(dr_measurement_data,times);
+% gyro_mag_heading_solution = dr_measurement_data(:,7);
 
 %% DR Solution
 dr_solution = Dead_Reckoning(dr_measurement_data,gnss_solution,gyro_mag_heading_solution,times);
@@ -28,11 +29,18 @@ dr_gnss_velocity_e = dr_gnss_solution(:,5);
 
 
 %% Plotting
+% figure;
+% plot(gnss_longitude,gnss_latitude);
+% hold on
+% plot(dr_longitude,dr_latitude);
+% hold on
+% plot(dr_gnss_longitude,dr_gnss_latitude);
+% legend(["GNSS","DR","GR-DNSS"]);
 
-% Plot_Graph(gnss_longitude,gnss_latitude,gnss_velocity_n,gnss_velocity_e,times,"GNSS",false)
+Plot_Graph(gnss_longitude,gnss_latitude,gnss_velocity_n,gnss_velocity_e,times,"GNSS",false)
 
-% Plot_Graph(dr_longitude,dr_latitude,dr_velocity_n,dr_velocity_e,times,"DR",false)
+Plot_Graph(dr_longitude,dr_latitude,dr_velocity_n,dr_velocity_e,times,"DR",false)
 
-% Plot_Graph(dr_gnss_longitude,dr_gnss_latitude,dr_gnss_velocity_n,dr_gnss_velocity_e,times,"DR-GNSS",false)
+Plot_Graph(dr_gnss_longitude,dr_gnss_latitude,dr_gnss_velocity_n,dr_gnss_velocity_e,times,"DR-GNSS",false)
 
-Plot_heading(dr_gnss_longitude,dr_gnss_latitude,gyro_mag_heading_solution,true);
+Plot_heading(dr_gnss_longitude,dr_gnss_latitude,gyro_mag_heading_solution,false);
